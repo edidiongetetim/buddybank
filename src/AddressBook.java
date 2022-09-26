844af1bb68439b476704f0570c596b55bc9ecbf0
Edidiong <edidiongetetim@gmail.com>
2022-09-26 16:38:42 -0400

    Modified the main method to add and remove buddies.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -10,13 +10,16 @@ public class AddressBook {
     private ArrayList<BuddyInfo> buddies;
 
     public AddressBook(){
+
         this.buddies = new ArrayList<>();
     }
     public void addBuddy(BuddyInfo buddy){
+
         this.buddies.add(buddy);
     }
 
     public void removeBuddy(BuddyInfo buddy){
+
         this.buddies.remove(buddy);
     }
 
